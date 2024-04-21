0b64d26073702f20c719a8354f9f55b1088b5f3a
unobu <grom72grim@yandex.ru>
2024-04-21 18:30:31 +0300

    Module

---
+++ PrimeService.Tests/PrimeService_IsPrimeShouldFlyoutMenuItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeService.Tests
+{
+    public class PrimeService_IsPrimeShouldFlyoutMenuItem
+    {
+        public PrimeService_IsPrimeShouldFlyoutMenuItem()
+        {
+            TargetType = typeof(PrimeService_IsPrimeShouldFlyoutMenuItem);
+        }
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public Type TargetType { get; set; }
+    }
+}
